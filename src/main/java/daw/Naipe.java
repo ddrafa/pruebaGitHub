9ddad9e04134487893967f979d94d3615d26be7b
rdiadel1008 <rdiadel1008@g.educaand.es>
2023-12-01 09:32:23 +0100

    Constructor por defecto

--- src/main/java/daw/Naipe.java
+++ src/main/java/daw/Naipe.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package daw;
 
 import java.util.Random;
@@ -15,4 +14,15 @@ public class Naipe {
     private int carta;
     private String palo;
 
+    public Naipe() {
+
+        Random rd = new Random();
+        this.carta = rd.nextInt(1, 11);
+        String[] palos = new String[4];
+        palos[0] = "oro";
+        palos[1] = "basto";
+        palos[2] = "espada";
+        palos[3] = "copa";
+        this.palo = palos[rd.nextInt(0, palos.length)];
+    }
 }
