47e8ba421bd0f56c1ed7f3afe4ccb3bc49cede7b
rdiadel1008 <rdiadel1008@g.educaand.es>
2023-12-01 09:22:44 +0100

    Inicio Proyecto

---
+++ src/main/java/daw/Naipe.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw;
+
+import java.util.Random;
+
+/**
+ *
+ * @author ddrafa
+ */
+public class Naipe {
+
+    private int carta;
+    private String palo;
+
+}
