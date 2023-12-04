d3c5f82ca91761197294b2311142365cdebbdf6b
rdiadel1008 <rdiadel1008@g.educaand.es>
2023-12-04 08:43:57 +0100

    Queda el array de números aleatorios

--- src/main/java/daw/Main.java
+++ src/main/java/daw/Main.java
@@ -4,13 +4,40 @@
  */
 package daw;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author ddrafa
  */
 public class Main {
+
     public static void main(String[] args) {
+        boolean repetir = false;
+        Scanner sc = new Scanner(System.in);
+
+        Naipe nA = new Naipe();
+        System.out.println(nA.toString());
+
         Baraja b1 = new Baraja();
-        System.out.print(b1.toString() + " - ");
+        System.out.println(b1.toString());
+
+        Naipe solicitante = new Naipe(1, "oro");
+        do {
+            try {
+                System.out.println("Introduce el número de tu carta:");
+                solicitante.setCarta(sc.nextInt());
+                repetir = false;
+            } catch (InputMismatchException ime) {
+                sc.nextLine();
+                System.out.println("Error pon un numero");
+                repetir = true;
+            }
+        } while (repetir);
+        sc.nextLine();
+        System.out.println("Introduce tu palo:");
+        solicitante.setPalo(sc.nextLine());
+        System.out.println(solicitante.toString());
     }
 }
